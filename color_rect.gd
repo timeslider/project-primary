5fefed524f8500792d2379a4447fe272a985ada0
Robert <timeslider86@gmail.com>
2025-02-11 19:13:45 -0500

    Daily backup

--- color_rect.gd
+++ color_rect.gd
@@ -1,5 +1,7 @@
 extends ColorRect
 
+const UI_THEME = preload("uid://dot0gi1yoqmrl")
+var tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,6 +10,15 @@ func _ready() -> void:
 	
 #	How to change a GradientTexture2D, could be useful changing themes
 	material.set("shader_parameter/gradient", preload("uid://ddwt5dglitg54") as GradientTexture2D) # UID = world 1
+
+	var tab_container_stylebox_flat: StyleBoxFlat = UI_THEME.get_stylebox("panel", "TabContainer")
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween()
+	tween.tween_interval(2.0)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_BOUNCE)
+	tween.tween_property(tab_container_stylebox_flat, "border_width_bottom", 50.0, 0.5)
 	
 
 
@@ -15,3 +26,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_tab_container_tab_hovered(tab: int) -> void:
+	if tween:
+		tween.kill()
+	var tab_container_stylebox_flat: StyleBoxFlat = UI_THEME.get_stylebox("tab_hovered", "TabContainer")
+	tab_container_stylebox_flat.border_color = Color(0.0, 0.0, 0.0)
+
+	tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(tab_container_stylebox_flat, "border_color", Color(5.0, 5.0, 5.0), 0.5)
