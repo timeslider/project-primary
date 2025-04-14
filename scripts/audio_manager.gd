0bc3cdc9dee753c0cda36248665313d8dd11ff0d
Robert <timeslider86@gmail.com>
2025-04-14 19:19:49 -0400

    Daily backup

---
+++ scripts/audio_manager.gd
@@ -0,0 +1,30 @@
+class_name AudioManager
+extends Node
+
+var sound_effect_dict = {}
+
+@export var sound_effect_settings: Array[SoundEffectSettings]
+
+func _ready() -> void:
+	for sound_effect_setting: SoundEffectSettings in sound_effect_settings:
+		sound_effect_dict[sound_effect_setting.type] = sound_effect_setting
+
+func create_2d_audio_at_location(location: Vector2, type: SoundEffectSettings):
+	if sound_effect_dict.has(type):
+		var sound_effect_setting: SoundEffectSettings = sound_effect_dict[type]
+		if sound_effect_setting.has_open_limit():
+			sound_effect_setting.change_audio_count(1)
+			var new_2d_audio = AudioStreamPlayer2D.new()
+			add_child(new_2d_audio)
+			
+			new_2d_audio.position = location
+			new_2d_audio.stream = sound_effect_setting.sound_effect
+			new_2d_audio.volume_db = sound_effect_setting.volume
+			new_2d_audio.pitch_scale = sound_effect_setting.pitch_scale
+			#new_2d_audio.pitch_scale += Global.rng.randf_range(-sound_effect_setting.pitch_randomness, sound_effect_setting.pitch_randomness)
+			new_2d_audio.finished.connect(sound_effect_setting.on_audio_finished)
+			new_2d_audio.finished.connect(new_2d_audio.queue_free)
+			
+			new_2d_audio.play()
+	else:
+		push_error("Audio Manager failed to find setting for type ", type)
---
+++ scripts/sound_effect_settings.gd
@@ -0,0 +1,28 @@
+class_name SoundEffectSettings
+extends Resource
+
+enum SoundEffectType {
+	ON_TILE_PLACED,
+	UI_HOVERED,
+	UI_BUTTON_PRESSED,
+}
+
+@export_range(0, 10) var limit: int = 5
+@export var type: SoundEffectType
+@export var sound_effect: AudioStreamMP3
+@export_range(-40, 20) var volume = 0
+@export_range(0.0, 4.0, 0.01) var pitch_scale = 1.0
+@export_range(0.0, 1.0, 0.1) var pitch_randomness = 0.0
+
+var audio_count = 0
+
+func change_audio_count(amount: int) -> void:
+	audio_count = max(0, audio_count + amount)
+
+
+func has_open_limit() -> bool:
+	return audio_count < limit
+
+
+func on_audio_finished() -> void:
+	change_audio_count(-1)
